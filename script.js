2e154d950498fae7a52f20eaf928e1c7c0a8f243
omkarmagadum-dev <omkar.magadum.dev@gmail.com>
2025-05-08 20:22:43 +0530

    learner DOM manipulation

---
+++ script.js
@@ -0,0 +1,23 @@
+//select and save
+var btn = document.querySelector('button');
+
+//selections types
+
+
+// //id
+// var abcd = document.querySelector('#abcd');
+
+// //class
+// var classabcd = document.querySelector('.abcd');
+
+// //tag
+// var h1 = document.querySelector('h1');
+
+// var h3 = document.querySelector("h3");
+// var allh3 = document.querySelectorAll("m3");
+
+btn.textContent += "Starting...";    
+btn.innerHTML = "<i>hello</i>";    // textcontent will print <i>hello</i> and innerHTML will print hello
+
+
+
