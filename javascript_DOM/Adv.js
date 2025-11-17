4bb7d2b6c80d3d80a3cc6cb99ad29ed6ffad70c3
Omkar Magadum <omkar.magadum.dev@gmail.com>
2025-11-17 21:20:25 +0530

    docs(js): document object property counting methods and Bash scripting basics

---
+++ javascript_DOM/Adv.js
@@ -0,0 +1,16 @@
+let products = {
+    name:"iphone17promax",
+    company:"apple",
+    myname :"omkar",
+    ceo:"tim",
+    price:15000
+}
+
+//Object.keys(product) 
+//Object.values(product)
+//Object.entries(product) return the array each 2 lenght 0 is key & 1 is the values 
+
+// how do you know the length ?? .length 
+ 
+
+// ++++++++++++++++++++++++++++++++ 
